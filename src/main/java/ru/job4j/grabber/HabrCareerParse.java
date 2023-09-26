bd26d05a34f77c45945a6d6d204dc0e6cd233496
Axiom <d-343@yandex.ru>
2023-09-26 13:02:59 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,13 +15,19 @@ public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static int page = 2;
 
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + i);
+        for (int i = 1; i <= page; i++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + page);
             System.out.println(i + " СТРАНИЦА");
             getJobs(connection);
         }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+        Document document = Jsoup.connect(link).get();
+        return document.select(".vacancy-description__text").first().text();
     }
 
     private static void getJobs(Connection connection) throws IOException {
@@ -36,7 +42,11 @@ public class HabrCareerParse {
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-mm-ddТhh:mm:ss");
             dataTimeOfVacancy = dateTimeParser.parse(dataTimeOfVacancy).format(dateTimeFormatter);
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s -- %s %s%n", dataTimeOfVacancy, vacancyName, link);
+            try {
+                System.out.printf("%s -- %s -- %s%n %s%n", dataTimeOfVacancy, vacancyName, link, retrieveDescription(link));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 }
